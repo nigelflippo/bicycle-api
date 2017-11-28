203d677a9cff58ff4c773a51b0ee359445742191
nigelflippo <flippo.nigel@gmail.com>
2017-11-28 07:43:40 -0700

    DB

--- db/bikes.js
+++ db/bikes.js
@@ -19,5 +19,12 @@ module.exports = [
     type: 'Fixed',
     year: '2008',
     condition: 'Fair'
+  },
+  {
+    id: '2d307188-0073-4d36-8f30-74e4efc687a4',
+    brand: 'Cervelo',
+    type: 'Triathlon',
+    year: '2017',
+    condition: 'New'
   }
 ]
