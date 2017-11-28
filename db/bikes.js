b29f38b7b0359217e85f2dcf4978d2dcf3d01388
nigelflippo <flippo.nigel@gmail.com>
2017-11-28 07:18:04 -0700

    Added ID fix

--- db/bikes.js
+++ db/bikes.js
@@ -1,16 +1,23 @@
 module.exports = [
   {
-    id: 1,
+    id: '040f873d-df27-4f5c-8161-25ac0357ea35',
     brand: 'Trek',
     type: 'Mountain',
     year: '2014',
     condition: 'Good'
   },
   {
-    id: 2,
+    id: 'a60ef22f-e3e6-47c6-9171-33848a63fa50',
     brand: 'Specialized',
     type: 'Road',
     year: '2012',
     condition: 'Excellent'
+  },
+  {
+    id: 'a600d137-9362-4636-ad56-be45a5c19816',
+    brand: 'Cinelli',
+    type: 'Fixed',
+    year: '2008',
+    condition: 'Fair'
   }
 ]
